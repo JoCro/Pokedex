c78f466631865ed31190f881db58b737ad657bd0
Johannes Kroh <avogath@gmail.com>
2024-04-17 18:46:23 +0200

    code cleaning

--- script.js
+++ script.js
@@ -8,24 +8,40 @@ let indexOfSinglePokemonDiv = [];
 
 
 async function loadThemAll(){ //lädt die ersten 20 Pokemon der API 
-    
-    for (let i = 0; i <= amountToLoad; i++) { //für 20x wird folgender code ausgeführt
-        const element = i +1;
+    for (let i = 1; i <= amountToLoad; i++) { //für 20x wird folgender code ausgeführt
+        const element = i;
         let url = `https://pokeapi.co/api/v2/pokemon/${element}` //die URL wird auf die PokeAPI gesetzt und das Pokemon mit der ID (element == der index) wird geladen
         let response = await fetch(url); //Es wird gewartet, bis das pokemon von der API fertig geladen ist. Dieses wird dann in die Variable response gespeichert.
         currentPokemon = await response.json(); // wenn response(das geladene Pokemon) in ein JSON formatiert wurde, wird dieses JSON in currentPokemon gespeichert.
-        
-        
-        indexOfSinglePokemonDiv.push(element); //Der ehem. indexwert (element) wird in das array gespeichert. element ist ebenfalls die id des Pokemon
-        document.getElementById('pokemon-gallery').innerHTML += `<div id="${currentPokemon['name']}" onclick="loadPokemon('${element}')" class="single-pokemon ${currentPokemon['types'][0]['type']['name']}"><h2>${currentPokemon['name']}</h2><div class="types-container" id="typesOf${currentPokemon['name']}"></div><img src="${currentPokemon['sprites']['other']['home']['front_shiny']}"></div>`;
-        
-        for (let j = 0; j < currentPokemon['types'].length; j++) { //für jeden typ des Pokemon wird folgender Vorgang wiederholt
-            const element = currentPokemon['types'][j]['type']['name']; // eine Konstante mit dem Inhalt des Typs wird angelegt
-            document.getElementById(`typesOf${currentPokemon['name']}`).innerHTML += `<div class="type">${element}</div>` //dem Pokemon wird der Typ in einem Div übergeben
-        }
-        
+        displayThePokemon(element);  
     }
 }
+
+
+async function loadMorePokemon(){
+    index = amountToLoad +1; //die Variable index wird zu "21", damit das nächste Pokemon, das aus der API geladen wird, das mit der ID 21 ist.
+    amountToLoad += 20; //Die Obergrenze für die geladenen Pokemon wird auf 40 erhöht
+
+    for (let i = index; i <= amountToLoad; i++) { //folgendes wird 20 mal wiederholt.
+        const element = i;
+        let url = `https://pokeapi.co/api/v2/pokemon/${element}`
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+       displayThePokemon(element);
+    }
+}
+
+
+function displayThePokemon(element){
+    indexOfSinglePokemonDiv.push(element); //Der ehem. indexwert (element) wird in das array gespeichert. element ist ebenfalls die id des Pokemon
+    document.getElementById('pokemon-gallery').innerHTML += `<div id="${currentPokemon['name']}" onclick="loadPokemon('${element}')" class="single-pokemon ${currentPokemon['types'][0]['type']['name']}"><h2>${currentPokemon['name']}</h2><div class="types-container" id="typesOf${currentPokemon['name']}"></div><img src="${currentPokemon['sprites']['other']['home']['front_shiny']}"></div>`;
+    
+    for (let j = 0; j < currentPokemon['types'].length; j++) { //für jeden typ des Pokemon wird folgender Vorgang wiederholt
+        const element = currentPokemon['types'][j]['type']['name']; // eine Konstante mit dem Inhalt des Typs wird angelegt
+        document.getElementById(`typesOf${currentPokemon['name']}`).innerHTML += `<div class="type">${element}</div>` //dem Pokemon wird der Typ in einem Div übergeben
+    }
+}
+
 
 function filterPokemon(){ // Pokemon sollen basierend auf einem Input (text) gefiltert und angezeigt werden. 
     
@@ -38,10 +54,10 @@ function filterPokemon(){ // Pokemon sollen basierend auf einem Input (text) gef
         }else{
             containers[i].style.display = 'flex';//ansonsten wird er angezeigt
         }
-        
     }
-    document.getElementById('more-btn').style.display = 'none'; //nachdem gefiltert wurde, wird der Button, der weitere Pokemon lädt, ausgeblendet
+    displayNone('more-btn'); //nachdem gefiltert wurde, wird der Button, der weitere Pokemon lädt, ausgeblendet
 }
+
 
 function resetFilters(){ //Diese Funktion hebt die Filterung der Pokemon auf
     let containers = document.getElementsByClassName('single-pokemon'); //eine variable wird angelegt und sie wird mit allen einzelnen pokemon befüllt, die geladen wurden 
@@ -53,69 +69,50 @@ function resetFilters(){ //Diese Funktion hebt die Filterung der Pokemon auf
     document.getElementById('more-btn').style.display = 'block'; //Der Button, der noch mehr Pokemon lädt, wird wieder angezeigt.
 }
 
-async function loadMorePokemon(){
-    index = amountToLoad +1; //die Variable index wird zu "21", damit das nächste Pokemon, das aus der API geladen wird, das mit der ID 21 ist.
-    amountToLoad += 20; //Die Obergrenze für die geladenen Pokemon wird auf 40 erhöht
-    for (let i = index; i <= amountToLoad; i++) { //folgendes wird 20 mal wiederholt.
-        const element = i +1;
-        let url = `https://pokeapi.co/api/v2/pokemon/${element}`
-        let response = await fetch(url);
-        currentPokemon = await response.json();
-        
-        indexOfSinglePokemonDiv.push(element);
-        document.getElementById('pokemon-gallery').innerHTML += `<div id="${currentPokemon['name']}" onclick="loadPokemon('${element}')" class="single-pokemon ${currentPokemon['types'][0]['type']['name']}"><h2>${currentPokemon['name']}</h2><div class="types-container" id="typesOf${currentPokemon['name']}"></div><img src="${currentPokemon['sprites']['other']['home']['front_shiny']}"></div>`;
-        for (let j = 0; j < currentPokemon['types'].length; j++) {
-            const element = currentPokemon['types'][j]['type']['name'];
-            document.getElementById(`typesOf${currentPokemon['name']}`).innerHTML += `<div class="type">${element}</div>`
-        }
-    }
-}
 
-
-async function loadPokemon(name){ //diese funktion lädt die detailansicht des Pokemons, das als Funktionsparameter eingegeben wird. Die ID des Pokemon
+async function loadPokemon(name){ //diese funktion lädt die DetailView des Pokemons, das als Funktionsparameter eingegeben wird. Die ID des Pokemon
     
     document.getElementById('detail-view').classList.remove('hidden'); //Die DetailView wird angezeigt
-    
     let url = `https://pokeapi.co/api/v2/pokemon/${name}`; 
     let response = await fetch(url);
     detailPokemon = await response.json();
-    
-    
-    
-    
     renderPokemonInfo(name); //Die funktion wird mit dem Funktionsparameter name = ID des Pokemon aufgerufen
 }
 
+
 function renderPokemonInfo(name){ //Diese Funktion soll die Stats des Pokemon laden, dessen ID als Funktionsparameter eingegeben wurde 
+    getTheElementsForDetailView(name);
     
-    
+    for(let j = 0; j < detailPokemon['types'].length; j++) { // Die Typen des Pokemon werden gerendert 
+        
+        document.getElementById('types').innerHTML += `<div class="types">${detailPokemon['types'][j]['type']['name']}</div>`;
+    }
+
+    renderStatsForDetailView(); // Für das aktuelle Pokemon wird hiermit die Stats geladen
+    gimmeTheChart();
+}
+
+
+function getTheElementsForDetailView(name){
     document.getElementById('left-btn').innerHTML = `<button class="ctrl-btn" onclick="checkNextToLoad('minus', ${name})"><p><</p></button>`;
     document.getElementById('right-btn').innerHTML = `<button class="ctrl-btn" onclick="checkNextToLoad('plus', ${name})"><p>></p></button>`;
     document.getElementById('info-container').innerHTML = ``;
     document.getElementById('pokemonName').innerHTML = detailPokemon['name'];
     document.getElementById('pokemonPic').src = detailPokemon['sprites']['other']['home']['front_shiny'];
     document.getElementById('types').innerHTML = ``;
-    
-    for(let j = 0; j < detailPokemon['types'].length; j++) { // Die Typen des Pokemon werden gerendert 
-        
-        document.getElementById('types').innerHTML += `<div class="types">${detailPokemon['types'][j]['type']['name']}</div>`;
-    }
-    
-    
-  renderStatsForDetailView();
-    
-    document.getElementById('info-container').innerHTML += `<div class="canvas-div">
-    <canvas id="myChart"></canvas>
-    </div>`
-    
+}
+
+
+function gimmeTheChart(){
+    document.getElementById('info-container').innerHTML += `<div class="canvas-div"><canvas id="myChart"></canvas></div>`
     drawChart();
     statNames = [];
     statvalues = [];
     document.getElementById('pokedex').classList = ``;
-    document.getElementById('pokedex').classList.add(`${detailPokemon['types'][0]['type']['name']}`);
-    document.body.style.overflowY = 'hidden';
-    
+    document.getElementById('pokedex').classList.add(`${detailPokemon['types'][0]['type']['name']}`); //Dem Pokemon wird die Klasse anhand seines 1. Typs zugewiesen.
+    document.body.style.overflowY = 'hidden'; //Das Scrollen wird verhindert
 }
+
 
 function renderStatsForDetailView(){
     for (let i = 0; i < detailPokemon['stats'].length; i++) { // für jeden Stat des Pokemon wird folgendes ausgeführt
@@ -126,31 +123,26 @@ function renderStatsForDetailView(){
     }
 }
 
-function checkNextToLoad(scale, pokemon){
+
+function checkNextToLoad(scale, pokemon) {
+    let totalPokemon = indexOfSinglePokemonDiv.length;
     
-    if(scale == 'minus'){
-        if(pokemon > 1){
-            pokemon -= 1;
-        }else{
-            pokemon = indexOfSinglePokemonDiv.length;
-        };
-        
-    }else{
-        if(pokemon == indexOfSinglePokemonDiv.length){
-            pokemon = 1;
-        }else{
-            pokemon += 1;
-        }
-        
+    if (scale === 'minus') {
+        pokemon = (pokemon > 1) ? pokemon - 1 : totalPokemon;
+    } else {
+        pokemon = (pokemon < totalPokemon) ? pokemon + 1 : 1;
     }
-    
-    
-    
     loadPokemon(pokemon);
-    
 }
 
-function closeDetail(){
-    document.getElementById('detail-view').classList.add('hidden');
-    document.body.style.overflowY = 'unset'
+
+
+function closeDetail(){ //diese funktion, soll die DetailView schließen
+    document.getElementById('detail-view').classList.add('hidden'); //Der DetailView wird die Klasse hidden übergeben = display:none
+    document.body.style.overflowY = 'unset' //Es kann wieder gescrollt werden.
+}
+
+
+function displayNone(object){
+    document.getElementById(object).style.display = 'none';
 }
